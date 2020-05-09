b4d872ff37d60e14f8aae0e5419f60cf266b9ad1
codebymatt <mattcraig365@gmail.com>
2020-05-09 23:22:40 +0100

    Lightly refactor AST Generator.

--- tool/generate_ast.rb
+++ tool/generate_ast.rb
@@ -14,7 +14,7 @@ class GenerateAST
     end
 
     output_directory = @args[0]
-    define_ast(output_directory, 'expr', expression_type_array)
+    define_ast(output_directory, 'Expr', expression_type_array)
   end
 
   def expression_type_array
@@ -27,40 +27,43 @@ class GenerateAST
   end
 
   def define_ast(output_directory, base_class_name, types)
-    path = "./#{output_directory}/#{base_class_name}.rb"
+    @base_class_name = base_class_name
+    path = "./#{output_directory}/#{@base_class_name.downcase}.rb"
     @output_file = File.open(path, 'w')
 
     @output_file.puts('# frozen_string_literal: true')
     @output_file.puts('')
 
-    @output_file.puts("class #{base_class_name.capitalize}")
+    @output_file.puts("class #{@base_class_name}")
     @output_file.puts('end')
     @output_file.puts('')
 
-    types.each do |type|
-      sub_class_name = type.split(':')[0].strip
-      fields = type.split(':')[1].strip
-      define_type(base_class_name.capitalize, sub_class_name, fields)
-    end
+    types.each { |type| prepare_and_generate_type(type) }
 
     @output_file.close
   end
 
-  def define_type(base_class_name, sub_class_name, fields)
-    write_class_opening(base_class_name, sub_class_name, fields)
+  def prepare_and_generate_type(type)
+    sub_class_name = type.split(':')[0].strip
+    fields = type.split(':')[1].strip
+    define_type(sub_class_name, fields)
+  end
+
+  def define_type(sub_class_name, fields)
+    write_class_opening(sub_class_name, fields)
 
     fields.split(' ')
           .map { |field| field.gsub(',', '') }
           .each { |field| write_type(field) }
 
-    write_visitor_pattern(base_class_name, sub_class_name)
+    write_visitor_pattern(sub_class_name)
     write_class_ending
   end
 
-  def write_class_opening(base_class_name, sub_class_name, fields)
+  def write_class_opening(sub_class_name, fields)
     capitalized_sub_class = sub_class_name.capitalize
     @output_file.puts "# Responsible for #{capitalized_sub_class} expression"
-    @output_file.puts("class #{sub_class_name} < #{base_class_name}")
+    @output_file.puts("class #{sub_class_name} < #{@base_class_name}")
     @output_file.puts("  attr_accessor #{accessors_from(fields)}")
     @output_file.puts ''
     @output_file.puts("  def initialize(#{fields})")
@@ -71,8 +74,8 @@ class GenerateAST
     @output_file.puts("    @#{trimmed_field} = #{trimmed_field}")
   end
 
-  def write_visitor_pattern(base_class_name, sub_class_name)
-    visitor_target = "#{sub_class_name.capitalize}#{base_class_name.capitalize}"
+  def write_visitor_pattern(sub_class_name)
+    visitor_target = "#{sub_class_name.capitalize}#{@base_class_name}"
     @output_file.puts('')
     @output_file.puts('  def accept(visitor)')
     @output_file.puts("    visitor.visit#{visitor_target}(self)")
