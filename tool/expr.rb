94e79934928060c58ed3e667afaace9322c688a2
codebymatt <mattcraig365@gmail.com>
2020-05-09 23:15:01 +0100

    Add AST Generator: generate classes for different expression types.

---
+++ tool/expr.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Expr
+end
+
+# Responsible for Binary expression
+class Binary < Expr
+  attr_accessor :left, :operator, :right
+
+  def initialize(left, operator, right)
+    @left = left
+    @operator = operator
+    @right = right
+
+  def accept(visitor)
+    visitor.visitBinaryExpr(self)
+  end
+  end
+end
+
+# Responsible for Grouping expression
+class Grouping < Expr
+  attr_accessor :expression
+
+  def initialize(expression)
+    @expression = expression
+
+  def accept(visitor)
+    visitor.visitGroupingExpr(self)
+  end
+  end
+end
+
+# Responsible for Literal expression
+class Literal < Expr
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+
+  def accept(visitor)
+    visitor.visitLiteralExpr(self)
+  end
+  end
+end
+
+# Responsible for Unary expression
+class Unary < Expr
+  attr_accessor :operator, :right
+
+  def initialize(operator, right)
+    @operator = operator
+    @right = right
+
+  def accept(visitor)
+    visitor.visitUnaryExpr(self)
+  end
+  end
+end
+
---
+++ tool/generate_ast.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Dynamically generate AST for Rox
+class GenerateAST
+  def initialize(args)
+    @args = args
+  end
+
+  def run
+    if @args.length != 1
+      puts 'Usage: ./generate_ast<output_directory>'
+      exit(64)
+    end
+
+    output_directory = @args[0]
+    define_ast(output_directory, 'expr', expression_type_array)
+  end
+
+  def expression_type_array
+    [
+      'Binary: left, operator, right',
+      'Grouping: expression',
+      'Literal: value',
+      'Unary: operator, right'
+    ]
+  end
+
+  def define_ast(output_directory, base_class_name, types)
+    path = "./#{output_directory}/#{base_class_name}.rb"
+    @output_file = File.open(path, 'w')
+
+    @output_file.puts('# frozen_string_literal: true')
+    @output_file.puts('')
+
+    @output_file.puts("class #{base_class_name.capitalize}")
+    @output_file.puts('end')
+    @output_file.puts('')
+
+    types.each do |type|
+      sub_class_name = type.split(':')[0].strip
+      fields = type.split(':')[1].strip
+      define_type(base_class_name.capitalize, sub_class_name, fields)
+    end
+
+    @output_file.close
+  end
+
+  def define_type(base_class_name, sub_class_name, fields)
+    write_class_opening(base_class_name, sub_class_name, fields)
+
+    fields.split(' ')
+          .map { |field| field.gsub(',', '') }
+          .each { |field| write_type(field) }
+
+    write_visitor_pattern(base_class_name, sub_class_name)
+    write_class_ending
+  end
+
+  def write_class_opening(base_class_name, sub_class_name, fields)
+    capitalized_sub_class = sub_class_name.capitalize
+    @output_file.puts "# Responsible for #{capitalized_sub_class} expression"
+    @output_file.puts("class #{sub_class_name} < #{base_class_name}")
+    @output_file.puts("  attr_accessor #{accessors_from(fields)}")
+    @output_file.puts ''
+    @output_file.puts("  def initialize(#{fields})")
+  end
+
+  def write_type(field)
+    trimmed_field = field.gsub(',', '')
+    @output_file.puts("    @#{trimmed_field} = #{trimmed_field}")
+  end
+
+  def write_visitor_pattern(base_class_name, sub_class_name)
+    visitor_target = "#{sub_class_name.capitalize}#{base_class_name.capitalize}"
+    @output_file.puts('')
+    @output_file.puts('  def accept(visitor)')
+    @output_file.puts("    visitor.visit#{visitor_target}(self)")
+    @output_file.puts('  end')
+  end
+
+  def write_class_ending
+    @output_file.puts('  end')
+    @output_file.puts('end')
+    @output_file.puts('')
+  end
+
+  def accessors_from(fields)
+    fields.split(', ').map { |f| ":#{f}" }.join(', ')
+  end
+end
+
+GenerateAST.new(ARGV).run
